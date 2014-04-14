711f7e34c64d628af406f0affc2159a06d543f9f
Matt Hartley <mbhartley1490@gmail.com>
2014-04-14 10:32:44 -0400

    need to add images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,36 @@
-console.log('\'Allo \'Allo!');
+
+$('.js-wfm-did').click(function(){
+	$('.js-hidden-did-wfm').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
+
+$('.js-wfm-learned').click(function(){
+	$('.js-hidden-learned-wfm').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
+
+$('.js-hgm-did').click(function(){
+	$('.js-hidden-did-hgm').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
+
+$('.js-hgm-learned').click(function(){
+	$('.js-hidden-learned-hgm').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
+
+$('.js-cc-did').click(function(){
+	$('.js-hidden-did-cc').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
+
+$('.js-cc-learned').click(function(){
+	$('.js-hidden-learned-cc').toggle(600);
+	$('html,body').animate({ scrollTop: $(this).offset().top - ( $(window).height() - $(this).outerHeight(true) ) / 2  }, 'slow');
+	return false;
+})
